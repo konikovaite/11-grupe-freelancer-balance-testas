02ba3398853f333a0cfedd10abef8755ec634243
konikovaite <konikovaite@gmail.com>
2019-11-28 15:50:20 +0200

    6

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,10 @@
 function renderlentele( list1, list2 ) {
     let HTML = '';
+    let iplaukos = 0;
+    let islaidos = 0;
+    let skirtumas = 0;
 
+    // menesiu rusiavimas is eiles
     for ( let i=0; i<list1.length; i++) {
         for ( let j=0; j<list1.length-1; j++) {
             if(list1[j].month > list1[j+1].month){
@@ -10,9 +14,16 @@ function renderlentele( list1, list2 ) {
             }
         }
     }
-    let iplaukos = 0;
-    let islaidos = 0;
-    let skirtumas = 0; 
+    
+    let m1 = list1[0].income;
+    let pav1 = list2[0];
+    let m2 = list1[0].income;
+    let pav2 = list2[0];
+    let m3 = list1[0].expense;
+    let pav3 = list2[0];
+    let m4 = list1[0].expense;
+    let pav4 = list2[0];
+
     for ( let i=0; i<list1.length; i++) {
         let parametrai = list1[i];    
         
@@ -52,6 +63,33 @@ function renderlentele( list1, list2 ) {
             skirtumas += balansas;
         }
 
+        // summary menesiu pavadinimai
+        // mėnuo, kai buvo mažiausiai uždirbta,bet ne lygu nuliui
+        if( parametrai.income < m1 &&
+            parametrai.income > 0){
+                m1 = parametrai.income;
+                pav1 = list2[i];
+        }
+        // mėnuo, kai buvo daugiausiai uždirbta
+        if( parametrai.income > m2){
+                m2 = parametrai.income;
+                pav2 = list2[i];
+        }
+        // mėnuo, kai buvo mažiausiai išlaidos, bet ne lygios nuliui
+        if( parametrai.expense < m3 &&
+            parametrai.expense > 0){
+                m3 = parametrai.expense;
+                pav3 = list2[i];
+        }
+        // mėnuo, kai buvo didžiausios išlaidos
+        if( parametrai.expense > m4){
+                m4 = parametrai.expense;
+                pav4 = list2[i];
+        }
+        
+
+
+
         // surenku lentele
         HTML += `<div class="table-row">
                     <div class="cell">${parametrai.month}</div>
@@ -72,6 +110,35 @@ function renderlentele( list1, list2 ) {
                 <div class="cell">${skirtumas} ${skirtumas  === '-' ? '' : 'Eur'}</div>`;
 
         document.querySelector('#footer').innerHTML = HTML;
-        }
+
+    HTML = '';
+        // summary
+        HTML = `<div class="item">
+                    <div class="value">${pav1}</div>
+                    <div class="title">mėnuo, kai buvo mažiausiai uždirbta,bet ne lygu nuliui</div>
+                </div>
+                <div class="item">
+                    <div class="value">${pav2}</div>
+                    <div class="title">mėnuo, kai buvo daugiausiai uždirbta</div>
+                </div>
+                <div class="item">
+                    <div class="value">${pav3}</div>
+                    <div class="title">mėnuo, kai buvo mažiausiai išlaidos, bet ne lygios nuliui</div>
+                </div>
+                <div class="item">
+                    <div class="value">${pav4}</div>
+                    <div class="title">mėnuo, kai buvo didžiausios išlaidos</div>
+                </div>`;
+
+        document.querySelector('#summary').innerHTML = HTML;
+
+
+
+
+
+
+
+
+}
 
 
